96c99cf9615c506a562ea46ac76454988fe14454
Filipe Torres <felipetorres2405@gmail.com>
2023-12-10 18:15:52 -0300

    Adicionando arquivo modelo de descricao da planta

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+
+function playAudio(){
+    document.querySelector("audio").play();
+    document.getElementById('autoplay-container').style.display = "none";
+}
+function stopAudio(){
+    document.getElementById('autoplay-container').style.display = "none";
+    localStorage.setItem("play", false);
+}
+setTimeout(() => {
+    if(localStorage.getItem("play") == true){
+        playAudio()
+    }else{
+        console.log("usuario rejeitou")
+    }
+  }, "10000");
